b2d77dbcd6a71cefd602543b9e16da0da597be68
Perzival1312 <jj.mongoose11@hotmail.com>
2018-11-12 08:18:06 -0800

    cleaning up a lil bit

--- controllers/codeSamples.js
+++ controllers/codeSamples.js
@@ -4,44 +4,31 @@ const router = express.Router();
 
 // INDEX
 router.get('/', (req, res) => {
-  console.log(req.path)
   res.render('index', {name: 'Jeremy'})
-
 });
 
 router.get('/codeSamples/new', (req,res) => {
-  console.log(req.path)
   res.render('index', {name: req.path})
 });
 
 router.post('/codeSamples', (req, res) => {
-  console.log(req.path)
   res.render('index', {name: req.path})
 });
 
 router.get('/codeSamples/:id', (req, res) => {
-  console.log(req.path)
   res.render('index', {name: req.path})
 });
 
 router.get('/codeSamples/:id/edit', (req, res) => {
-  console.log(req.path)
   res.render('index', {name: req.path})
 });
 
 router.patch('/codeSamples/:id', (req, res) => {
-  console.log(req.path)
   res.render('index', {name: req.path})
 });
 
 router.delete('/codeSamples/:id', (req, res) => {
-  console.log(req.path)
   res.render('index', {name: req.path})
 });
-// other routes go here
-// show singular
-// edit
-// DELETE
-
 
 module.exports = router
