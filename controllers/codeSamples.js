272d841ad30f35ce66f96a5f42627e4838354db8
Jayce Azua <jayce.azua@gmail.com>
2018-11-28 10:56:25 -0800

    updating

--- controllers/codeSamples.js
+++ controllers/codeSamples.js
@@ -6,9 +6,10 @@ const Code = require('../models/code');
 // INDEX
 router.get('/', (req, res) => {
   Code.find({}).then((codes) => {
+      console.log(codes)
     res.render('code-index', { codes });
   }).catch((err) => {
-    console.log(req.path, err.message);
+    res.send(err.message)
   })
 });
 // NEW
@@ -17,10 +18,10 @@ router.get('/codeSamples/new', (req,res) => {
 });
 // CREATE
 router.post('/codeSamples', (req, res) => {
-  Code.create(req.body).then((codes) => {
+  Code.create(req.body).then(() => {
     res.redirect('/');
-  }).catch((err )=> {
-    console.log(req.path, err.message);
+  }).catch((err)=> {
+    res.send(err.message)
   })
 });
 // SHOW
@@ -30,19 +31,6 @@ router.get('/codeSamples/:id', (req, res) => {
   }).catch(err => res.send(err.message))
 });
 
-// router.get('/codeSamples/:id', (req, res) => {
-//  // if (req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
-//  // Yes, it's a valid ObjectId, proceed with `findById` call.
-//    Code.findById(req.params.id).then(codes => {
-//      res.render('code-show', { codes });
-//    }).catch((err) => {
-//      console.log(req.path, err.message);
-//    });
-//  // } else{
-//    // console.log("the ID: ", req.params.id, "ain\'t right");
-//    // res.end();
-//  // }
-// });
 
 // EDIT
 router.get('/codeSamples/:id/edit', (req, res) => {
@@ -52,15 +40,14 @@ router.get('/codeSamples/:id/edit', (req, res) => {
     console.log(req.path, err.message)
   })
 });
+
 // UPDATE
-
-
 router.patch('/codeSamples/:id', (req, res) => { // put difference patch
   Code.findByIdAndUpdate(req.params.id, req.body).then((codes) => {
     res.redirect(`/codeSamples/${req.params.id}`);
-  }).catch(err => {
+  }).catch((err) => {
     // purpose of why you are using .method and .path
-    console.log(req.method, req.path, err.message);
+    res.send(err.message)
   });
 });
 
@@ -69,8 +56,8 @@ router.patch('/codeSamples/:id', (req, res) => { // put difference patch
 router.delete('/codeSamples/:id', (req, res) => {
   Code.findByIdAndRemove(req.params.id).then(() => {
     res.redirect("/")
-  }).catch(err => {
-    console.log(req.method, req.path, err.message)
+  }).catch((err) => {
+    res.send(err.message)
   })
 });
 
