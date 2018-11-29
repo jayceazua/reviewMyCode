b0d3f269dcf73cc2020c779e9761bb8ee1304610
Jayce Azua <jayce.azua@gmail.com>
2018-11-28 21:55:59 -0800

    fixing the mongo connection

--- database/mongooseConnection.js
+++ database/mongooseConnection.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
  // Set a Mongoose Promise Library
 mongoose.Promise = global.Promise;
-const dbname = "reviewMyCode";
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost:27017/${dbname}`;
-
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/code_review';
+// `mongodb://${dbuser}:${dbpassword}@ds115283.mlab.com:15283/${dbname}`
 mongoose.connect(dbURI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
-  console.log('Database is up!')
+  console.log('Rotten Potatoes\' database is up!')
 });
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'));
 mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
-// mongoose.set('debug', true);
+mongoose.set('useFindAndModify', false); // corrects the annoying DeprecationWarning
